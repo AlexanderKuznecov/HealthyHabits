63bab0aba750108cfb54b9b7f830bb2443360440
AlexanderKuznecov <alexander.kuznecov11@gmail.com>
2025-11-18 14:02:22 +0200

    added ui test

--- app/src/androidTest/java/com/example/healthyhabits/AddHabitScreenTest.kt
+++
@@ -1,4 +0,0 @@
-package com.example.healthyhabits
-
-class AddHabitScreenTest {
-}
---
+++ app/src/androidTest/java/com/example/healthyhabits/HabitUiTest.kt
@@ -0,0 +1,30 @@
+package com.example.healthyhabits
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.Rule
+import org.junit.Test
+
+class HabitUiTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun addHabitScreen_showsSaveButton() {
+        // Стартираме само AddHabitScreen в тестова Activity
+        composeTestRule.setContent {
+            AddHabitScreen(
+                onBackClick = {},
+                onSaveHabit = { _, _ -> }
+            )
+        }
+
+        // Проверяваме, че бутонът "Запази навика" се вижда на екрана
+        composeTestRule
+            .onNodeWithText("Запази навика")
+            .assertIsDisplayed()
+    }
+}
